0850043361ba922c61b9cf36b01e38d2e104a267
Arif Abul Hossain <arifabualhossain@gmail.com>
2019-07-04 18:57:25 +0600

    del

--- 04-07-19/hibernateInSpring/src/main/java/com/coderbd/hibernateInSpring/repo/DepartmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.coderbd.hibernateInSpring.repo;
-
-import com.coderbd.hibernateInSpring.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepo extends JpaRepository<Department, Long> {
-}
